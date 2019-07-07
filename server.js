11207c8ad7afb12415bb11703c38795ab117c6c0
Ivan Lopez <fuchi@live.com>
2019-07-07 01:18:24 -0500

    server actualizado

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: '.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { 
+  useCreateIndex: true,
+  useNewUrlParser: true 
+})
 .then(() => console.log('DB Connected'))
 .catch(err => console.log(err));
 
@@ -19,7 +24,11 @@ type Query {
 `;
 
 const server = new ApolloServer({
-  typeDefs, 
+  typeDefs,
+  context: {
+    User,
+    Post
+  } 
 });
 
 server.listen().then(({url}) => {
